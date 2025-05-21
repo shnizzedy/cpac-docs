8b96ddbd6b72befa4484f565e3eb6986669ef982
Tamsin Rogers <tamsinrog@gmail.com>
2025-05-21 16:12:39 -0400

    text setup for meta docs

--- assets/js/appendix.js
+++ assets/js/appendix.js
@@ -76,6 +76,6 @@
 
   window.addEventListener("load", () => {
     toggleScrolled();
-    loadYAML("../../assets/content/pages/appendix/appendix.yaml");
+    loadYAML("../assets/content/pages/appendix/appendix.yaml");
   });
 })();
