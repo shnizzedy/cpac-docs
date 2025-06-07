fd2edc810b1a242d24ef7a9c679b254eb9dc101e
Jon Cluce <jon.clucas@childmind.org>
2025-06-07 00:10:04 -0400

    :bug: Clean up versioning script

--- dev/collect-versions.sh
+++ dev/collect-versions.sh
@@ -3,10 +3,14 @@ DIR=${1:-./cpac-docs}
 
 # Collect clean top-level directories, excluding specific names
 mapfile -t dirs < <(
-  find "${DIR}" -mindepth 1 -maxdepth 1 -type d \
-  -exec basename {} \; |
-  grep -vE '^\.$' |
-  grep -vE '^(develop|latest|\.git|assets|pages)$'
+  for d in "$DIR"/*/; do
+    [ -d "$d" ] || continue
+    name=$(basename "$d")
+    case "$name" in
+      develop|latest|.git|assets|pages) continue ;;
+    esac
+    echo "$name"
+  done
 )
 
 # Sort version numbers
@@ -17,6 +21,8 @@ fi
 
 # Add 'latest' at the start and 'develop' at the end
 sorted_versions=("latest" "${sorted_versions[@]}" "develop")
+
 echo "Sorted versions: ${sorted_versions[*]}"
+
 # Output to versions.json
 printf "%s\n" "${sorted_versions[@]}" | jq -R . | jq -s . > "${DIR}/versions.json"
